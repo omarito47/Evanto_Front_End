87a9280e728f57ea4c7c03cfc9282c9767cfcf81
garfamaryem <maryembengarfa@gmail.com>
2024-06-13 01:37:07 +0100

    add category crud

---
+++ src/app/CategoryComponents/add-category/add-category.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CategoryService } from '../../core/services/category/category.service';
+import { Category } from '../../core/models/category';
+@Component({
+  selector: 'app-add-category',
+  templateUrl: './add-category.component.html',
+  styleUrls: ['./add-category.component.css'],
+})
+export class AddCategoryComponent {
+  id!: string;
+  cat!: Category;
+  selectedFile: File | null = null;
+
+  constructor(
+    private catService: CategoryService,
+    private router: Router,
+    private ar: ActivatedRoute
+  ) {
+    this.id = this.ar.snapshot.params['id'];
+    if (this.id != undefined) {
+      this.catService.getCategoryById(this.id).subscribe({
+        next: (data) => {
+          this.category.patchValue({
+            name: data.name,
+            description: data.description,
+            image: data.image,
+          });
+          this.cat = data;
+        },
+      });
+    }
+  }
+  category: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    description: new FormControl(),
+    image: new FormControl(''),
+  });
+
+  add() {
+    this.id != undefined
+      ? this.catService.updateCategory(this.id, this.category.value).subscribe({
+          next: () => this.router.navigate(['/category']),
+        })
+      : console.log(this.category.value);
+    this.catService.addCategory(this.category.value).subscribe({
+      next: () => this.router.navigate(['/category']),
+      error: (e) => alert(e.message),
+    });
+  }
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0] as File;
+    this.category.patchValue({ image: this.selectedFile });
+  }
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AuthRoutingModule } from './modules/auth/auth-routing.module';
+
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
-import { OrderComponent } from './OrderComponents/order/order.component';
 const routes: Routes = [
   {
     path: 'auth',
